5b7a1235a1abf5b2773b456c99503293a6dce1d6
fajar sidik priatna <jardik.oryza@gmail.com>
2023-09-17 23:04:45 +0700

    fix click

--- src/components/PersonalProject.svelte
+++ src/components/PersonalProject.svelte
@@ -8,19 +8,32 @@
     <div class="flex flex-row flex-wrap">
       {#each personalProjects as project}
         <!-- tech -->
-        <!-- svelte-ignore a11y-click-events-have-key-events -->
-        <!-- svelte-ignore a11y-no-static-element-interactions -->
         <div
-          class="border border-slate-300 shadow-lg overflow-hidden rounded-lg w-1/8 hover:cursor-pointer m-2"
-          on:click={() => {
-            if (project.link != "#") {
-              window.open(project.link, "_blank");
-            }
-          }}
+          class="border border-slate-300 shadow-lg overflow-hidden rounded-lg w-1/8 m-2"
         >
-          <img src={project.image} alt="laravel" class="w-full max-h-64" />
+          <!-- svelte-ignore a11y-click-events-have-key-events -->
+          <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+          <img
+            src={project.image}
+            alt="laravel"
+            class="w-full max-h-64 hover:cursor-pointer"
+            on:click={() => {
+              if (project.link != "#") {
+                window.open(project.link, "_blank");
+              }
+            }}
+          />
+          <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
           <div class="p-2">
-            <p class="text-xl text-wrap">
+            <!-- svelte-ignore a11y-click-events-have-key-events -->
+            <p
+              class="text-xl text-wrap hover:cursor-pointer"
+              on:click={() => {
+                if (project.link != "#") {
+                  window.open(project.link, "_blank");
+                }
+              }}
+            >
               {project.title}
             </p>
             <a href={project.repo} target="_blank">
